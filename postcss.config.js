4e929fe67118a1324fd688ae24597cc72e414ad8
Soumya Ranjan Hembram <soumyahembra123@gmail.com>
2024-10-10 14:14:28 +0530

    postcss.config.js and tailwind.config.js is created and updated

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+    plugins: [
+        require('autoprefixer'),
+        require('postcss-nested'),
+        // Add more PostCSS plugins as needed
+    ],
+};
