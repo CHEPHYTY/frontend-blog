ce726d1fe3231223ea08f686fe6f247aa69ff85e
Soumya Ranjan Hembram <soumyahembra123@gmail.com>
2024-10-12 16:30:09 +0530

    Blog editor code is added

---
+++ src/components/publish-form.component.jsx
@@ -0,0 +1,7 @@
+const PublishForm = () => {
+    return (
+        <h1>Publish From Component JSX file</h1>
+    )
+}
+
+export default PublishForm;
--- src/pages/editor.pages.jsx
+++ src/pages/editor.pages.jsx
@@ -1,21 +1,22 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../App";
 import { Navigate } from "react-router-dom";
+import BlogEditor from "../components/blog-editor.component";
+import PublishForm from "../components/publish-form.component";
 const Editor = () => {
 
 
-    const [editorState, setEditorState] = useState(true);
+    const [editorState, setEditorState] = useState("editor");
     let { userAuth: { accessToken } } = useContext(UserContext);
 
 
     return (
 
         accessToken === null ?
-
             <Navigate to="/signin" /> :
 
             editorState === "editor" ?
-                <h1>Blog Editor</h1> : <h1> Publish Form</h1>
+                <BlogEditor /> : <PublishForm />
     );
 }
 
